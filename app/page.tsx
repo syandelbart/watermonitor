e12e334092ac4a584efe808d54cb445ab0c2e180
Syan Delbart <syan@syandelbart.com>
2024-01-23 11:23:20 +0100

    use client

--- app/page.tsx
+++ app/page.tsx
@@ -1,60 +1,57 @@
-import Image from "next/image";
-import Link from 'next/link';
-import initializeApp  from "firebase/app";
+"use client";
+
+import { createUserWithEmailAndPassword, getAuth, signInWithEmailAndPassword } from 'firebase/auth';
 import { useState } from 'react';
-import { useRouter } from 'next/router';
-import { createUserWithEmailAndPassword } from "firebase/auth";
-import { getAuth, signInWithEmailAndPassword } from 'firebase/auth';
 
 const auth = getAuth();
 
 export default function Home() {
   const [value, setValue] = useState({
-    email: '',
-    password: '',
-    error: ''
-  })
+    email: "",
+    password: "",
+    error: "",
+  });
 
   async function signIn() {
-    if (value.email === '' || value.password === '') {
+    if (value.email === "" || value.password === "") {
       setValue({
         ...value,
-        error: 'Email and password are mandatory.'
-      })
+        error: "Email and password are mandatory.",
+      });
       return;
     }
 
     try {
       await signInWithEmailAndPassword(auth, value.email, value.password);
-    } catch (error) {
-    }
+    } catch (error) {}
   }
 
   async function signUp() {
-    if (value.email === '' || value.password === '') {
+    if (value.email === "" || value.password === "") {
       setValue({
         ...value,
-        error: 'Email and password are mandatory.'
-      })
+        error: "Email and password are mandatory.",
+      });
       return;
     }
 
     try {
       await createUserWithEmailAndPassword(auth, value.email, value.password);
-    } catch (error) {
-    
-    }
+    } catch (error) {}
   }
 
   return (
     <main className="justify-center items-center">
-        <h1 className="m-10 flex justify-center items-center">HomePage</h1>
+      <h1 className="m-10 flex justify-center items-center">HomePage</h1>
 
-
-        <div className="h-screen flex items-center justify-center">
-          <button className="m-3 bg-transparent hover:bg-blue-500 text-blue-700 font-semibold hover:text-white py-2 px-4 border border-blue-500 hover:border-transparent rounded">Login</button>
-          <button className="m-3 bg-transparent hover:bg-blue-500 text-blue-700 font-semibold hover:text-white py-2 px-4 border border-blue-500 hover:border-transparent rounded">SignUp</button>
-        </div>
+      <div className="h-screen flex items-center justify-center">
+        <button className="m-3 bg-transparent hover:bg-blue-500 text-blue-700 font-semibold hover:text-white py-2 px-4 border border-blue-500 hover:border-transparent rounded">
+          Login
+        </button>
+        <button className="m-3 bg-transparent hover:bg-blue-500 text-blue-700 font-semibold hover:text-white py-2 px-4 border border-blue-500 hover:border-transparent rounded">
+          SignUp
+        </button>
+      </div>
     </main>
   );
 }
