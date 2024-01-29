b79c6f01d879922d5b28734da03e749f1a43679b
Syan Delbart <syan@syandelbart.com>
2024-01-29 09:16:37 +0100

    Refactor component imports in layout files

--- app/dashboard/layout.tsx
+++ app/dashboard/layout.tsx
@@ -1,11 +1,13 @@
-import Protected from "../componets/Protected";
-import Navigation from "../componets/NavBar";
+import Navigation from "../components/NavBar";
+import Protected from "../components/Protected";
 
 const DashboardLayout = ({ children }: { children: React.ReactNode }) => {
-  return <Protected>
-    <Navigation />
-    {children}
-    </Protected>;
+  return (
+    <Protected>
+      <Navigation />
+      {children}
+    </Protected>
+  );
 };
 
 export default DashboardLayout;
