9cc6c09e0aef56862f00d198f34581672a06908c
Syan Delbart <syan@syandelbart.com>
2024-01-29 10:31:37 +0100

    Refactor Home component to fetch stations and display them in Dashboard

--- app/dashboard/home/page.tsx
+++ app/dashboard/home/page.tsx
@@ -1,14 +1,46 @@
-"use client";
-
-import { useRouter } from "next/navigation";
 import { Label } from "reactstrap";
 
-export default function Home() {
-  const router = useRouter();
+import Protected from "@/app/components/Protected";
+import { Station } from "@/types/general";
+
+import Dashboard from "./Dashboard";
+
+const Home = async () => {
+  let stations: Station[];
+
+  try {
+    const stationsFetched = await fetch(
+      `${process.env.NODE_RED_API}/stations`,
+      {
+        method: "GET",
+        // API_USERNAME and API_PASSWORD are set in .env.local
+        headers: new Headers({
+          Authorization:
+            "Basic " +
+            Buffer.from(
+              process.env.API_USERNAME + ":" + process.env.API_PASSWORD
+            ).toString("base64"),
+        }),
+      }
+    );
+
+    stations = await stationsFetched.json();
+  } catch (err: unknown) {
+    stations = [];
+    console.log(err);
+  }
 
   return (
-      <main className="flex m-10 flex-col">
-        <Label className="m-3 text-3xl items-start">WaterWatchers</Label>
-      </main>
+    <main className="flex m-10 flex-col">
+      <Label className="m-3 text-3xl items-start">WaterWatchers</Label>
+      <Protected>
+        <div>
+          <h1>Dashboard</h1>
+          <Dashboard stations={stations} />
+        </div>
+      </Protected>
+    </main>
   );
-}
+};
+
+export default Home;
