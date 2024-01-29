cb82da4e74528f32cb5a7dfcbef4c659ac388e7d
Syan Delbart <syan@syandelbart.com>
2024-01-29 10:30:22 +0100

    Remove Protected and Navigation components from Home page

--- app/page.tsx
+++ app/page.tsx
@@ -1,14 +1,8 @@
-import Protected from "./componets/Protected";
-import Navigation from "./componets/NavBar";
-
 const Home = () => {
   return (
-    <Protected>
-      <Navigation />
-      <div>
-        <h1>Home</h1>
-      </div>
-    </Protected>
+    <div>
+      <h1>Home</h1>
+    </div>
   );
 };
 
