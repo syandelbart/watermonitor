f7abe6904ca88ab3342a080ca694bbcfd7503f71
Syan Delbart <syan@syandelbart.com>
2024-01-31 16:02:27 +0100

    Add Zustand state management and sensor/measurement stores
    This commit adds a new file "zustand.ts" which contains the implementation of Zustand state management. It also includes the creation of two stores: SensorStore and MeasurementStore, which handle the management of sensor and measurement data respectively. (Only Sensor store is implemented currently)

---
+++ zustand.ts
@@ -0,0 +1,70 @@
+import { create } from "zustand";
+
+import { Measurement, NodeRedAuthHeaders, Sensor } from "./types/general";
+
+type SensorStore = {
+  sensors: Sensor[];
+  isFetchedInitial: boolean;
+  add: (sensor: Sensor) => void;
+  modify: (sensor: Sensor) => void;
+  remove: (sensorId: number) => void;
+  removeAll: () => void;
+  fetch: () => void;
+};
+
+export const useSensorStore = create<SensorStore>((set) => {
+  const state: SensorStore = {
+    sensors: [],
+    isFetchedInitial: true,
+    add: (sensor) => set((state) => ({ sensors: [...state.sensors, sensor] })),
+    remove: (sensorId) =>
+      set((state) => ({
+        sensors: state.sensors.filter((sensor) => sensor.id !== sensorId),
+      })),
+    modify: (sensor) =>
+      set((state) => ({
+        sensors: state.sensors.map((s) =>
+          sensor.id === s.id ? { ...sensor } : sensor
+        ),
+      })),
+    removeAll: () => set({ sensors: [] }),
+    fetch: async () => {
+      try {
+        const response = await fetch("/api/sensor", {
+          headers: NodeRedAuthHeaders,
+        });
+        const data: Sensor[] = await response.json();
+        set({ sensors: data, isFetchedInitial: true });
+      } catch (error) {
+        console.error("Failed to fetch sensors:", error);
+      }
+    },
+  };
+
+  (async () => {
+    // Call any actions or set any state you need here
+    await state.fetch();
+  })();
+
+  return state;
+});
+
+type MeasurementStore = {
+  measurements: Measurement[];
+  add: (measurement: Measurement) => void;
+  remove: (measurementId: number) => void;
+  removeAll: () => void;
+};
+
+export const useMeasurementStore = create<MeasurementStore>((set) => ({
+  measurements: [],
+  add: (measurement) =>
+    set((state) => ({ measurements: [...state.measurements, measurement] })),
+  remove: (measurementId) =>
+    set((state) => ({
+      measurements: state.measurements.filter(
+        (measurement) => measurement.id !== measurementId
+      ),
+    })),
+  removeAll: () => set({ measurements: [] }),
+}));
