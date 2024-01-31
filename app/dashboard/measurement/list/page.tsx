112c4016f064ff89233bedb2fb6eaa496920a0b0
Syan Delbart <syan@syandelbart.com>
2024-01-31 13:59:39 +0100

    Refactor measurement list page

--- app/dashboard/measurement/list/page.tsx
+++ app/dashboard/measurement/list/page.tsx
@@ -7,40 +7,42 @@ import {
   TableHeader,
   TableRow,
 } from "@/components/ui/table";
-import { NodeRedAuthHeaders, Sensor } from "@/types/general";
+import { Measurement, NodeRedAuthHeaders } from "@/types/general";
 
 const List = async () => {
-  const sensors: Sensor[] = await fetch(`${process.env.NODE_RED_API}/sensors`, {
-    headers: NodeRedAuthHeaders,
-  }).then((res) => res.json());
+  const headers = new Headers(NodeRedAuthHeaders);
+  headers.set("Cache-Control", "no-cache");
+  const measurements: Measurement[] = await fetch(
+    `${process.env.NODE_RED_API}/measurements`,
+    {
+      headers,
+    }
+  ).then((res) => res.json());
 
   return (
     <div>
-      <h1>Sensor List</h1>
+      <h1>Measurement List</h1>
       <div>
         <Table>
-          <TableCaption>A table of all the sensors in the system.</TableCaption>
+          <TableCaption>
+            A table of all the measurements in the system.
+          </TableCaption>
           <TableHeader>
             <TableRow>
               <TableHead>ID</TableHead>
-              <TableHead>Municipality</TableHead>
-              <TableHead>MAC Address</TableHead>
-              <TableHead>Station Name</TableHead>
-              <TableHead>(Longitude | Latitude)</TableHead>
+              <TableHead>Measurement</TableHead>
+              <TableHead>Moment</TableHead>
               <TableHead>Actions</TableHead>
             </TableRow>
           </TableHeader>
           <TableBody>
-            {sensors.map((sensor) => (
-              <TableRow key={sensor.id}>
-                <TableCell>{sensor.id}</TableCell>
-                <TableCell>{sensor.municipality}</TableCell>
-                <TableCell>{sensor.mac_address}</TableCell>
-                <TableCell>{sensor.station_name}</TableCell>
+            {measurements.map((measurement) => (
+              <TableRow key={measurement.id}>
+                <TableCell>{measurement.id}</TableCell>
+
+                <TableCell>{measurement.moment}</TableCell>
                 <TableCell>
-                  {sensor.longitude &&
-                    sensor.latitude &&
-                    `(${sensor.longitude},${sensor.latitude})`}
+                  {measurement.result} {measurement.measurement}
                 </TableCell>
                 <TableCell>{/* Actions */}</TableCell>
               </TableRow>
