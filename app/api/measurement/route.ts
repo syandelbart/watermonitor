7e9d1712810a5d28ae977baa23913cd0bbe19eb4
Syan Delbart <syan@syandelbart.com>
2024-02-01 09:11:15 +0100

    Add GET and DELETE methods to measurement route

--- app/api/measurement/route.ts
+++ app/api/measurement/route.ts
@@ -1,5 +1,31 @@
 import { Measurement, NodeRedAuthHeaders } from "@/types/general";
 
+const GET = async (request: Request) => {
+  const headers = new Headers(NodeRedAuthHeaders);
+  headers.set("Content-Type", "application/json");
+
+  const response = await fetch(`${process.env.NODE_RED_API}/measurements`, {
+    method: "GET",
+    headers: headers,
+  });
+
+  console.log(response);
+
+  try {
+    const data = await response.json();
+    console.log(data);
+    return new Response(JSON.stringify(data), {
+      headers: { "Content-Type": "application/json" },
+    });
+  } catch (error) {
+    console.log(error);
+  }
+  return new Response("Error", {
+    headers: { "Content-Type": "application/json" },
+    status: 500,
+  });
+};
+
 const POST = async (request: Request) => {
   const measurement = (await request.json()) as Measurement;
 
@@ -32,4 +58,29 @@ const POST = async (request: Request) => {
   });
 };
 
-export { POST };
+const DELETE = async (request: Request) => {
+  const measurement: Measurement = await request.json();
+
+  // TODO : Add field validation
+
+  const headers = new Headers(NodeRedAuthHeaders);
+  headers.set("Content-Type", "application/json");
+  const response = await fetch(`${process.env.NODE_RED_API}/measurement`, {
+    method: "DELETE",
+    headers: headers,
+    body: JSON.stringify({
+      id: measurement.id,
+    }),
+  });
+
+  try {
+    const data = await response.json();
+    return new Response(JSON.stringify(data), {
+      headers: { "Content-Type": "application/json" },
+    });
+  } catch (error) {
+    console.log(error);
+  }
+};
+
+export { GET, POST, DELETE };
